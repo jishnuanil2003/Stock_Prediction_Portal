8c51b186c29fd4e1d9cf49dde2076ab20efdc7af
Jishnuanil <aniljishnu07@gmail.com>
2025-09-29 21:06:39 +0530

    registration Page

--- backend_drf/accounts/serializers.py
+++ backend_drf/accounts/serializers.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only = True, style={'input_type':'password'})
+    password = serializers.CharField(write_only = True, style={'input_type':'password'}, min_length=6,)
     class Meta:
         model = User
         fields = ['username','password','email']
--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 import Header from './component/Header'
 import Body from './component/Body'
 import Footer from './component/Footer'
+import{BrowserRouter,Routes,Route} from "react-router-dom"
+import Register from './component/Register'
+import Login from './component/Login'
 const App = () => {
   return (
     <>
+    <BrowserRouter>
     <Header/>
-    <Body/>
+    <Routes>
+    <Route element={<Body/>} path='/'/>
+    <Route element={<Register/>} path='/register'/>
+    <Route element={<Login/>} path='/login'/>
+    </Routes>
     <Footer/>
+    </BrowserRouter>
     </>
   )
 }
